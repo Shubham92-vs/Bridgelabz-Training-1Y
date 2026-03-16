f8a12ca78d57d7274bd8c3039fbb8288df0db632
unknown <you@example.com>
2026-03-16 14:13:40 +0530

    [Shubham]

---
+++ Methods/src/com/gla/Level_2/UnitConvertorLength2.java
@@ -0,0 +1,23 @@
+
+package com.gla.Level_2;
+
+public class UnitConvertorLength2{
+
+    public static double yardsToFeet(double y)
+    {
+        return y*3;
+    }
+    public static double feetToYards(double f){
+        return f*0.333333;
+    }
+    public static double metersToInches(double m){
+        return m*39.3701;
+    }
+    public static double inchesToMeters(double i){
+        return i*0.0254;}
+
+    public static double inchesToCm(double i){
+        return i*2.54;
+    }
+}
+
