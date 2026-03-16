f24889fcf1637bc250312eee1eeb2532619a3280
unknown <you@example.com>
2026-03-16 14:12:40 +0530

    [Shubham]

---
+++ Methods/src/com/gla/Level_1/Handshakes.java
@@ -0,0 +1,15 @@
+
+package com.gla.Level1;
+
+import java.util.*;
+public class Handshakes{
+    public static int max(int n){
+        return n*(n-1)/2;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(max(n));
+    }
+}
+
---
+++ Methods/src/com/gla/Level_1/TriangularParkRounds.java
@@ -0,0 +1,18 @@
+
+package com.gla.Level1;
+
+import java.util.*;
+public class TriangularParkRounds{
+    public static double rounds(double a,double b,double c){
+        double p=a+b+c;
+        return 5000/p;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble(),b=sc.nextDouble(),c=sc.nextDouble();
+        System.out.println(rounds(a,b,c));
+    }
+}
+
+
+
